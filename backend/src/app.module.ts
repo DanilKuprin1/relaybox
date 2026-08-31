50f9005a4de43a2d7118c6b02f53bddd8aac90dc
DanilKuprin1 <danilkuprinca@gmail.com>
2026-08-31 15:32:37 -0400

    feat(backend): add zod request validation via nestjs-zod

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,44 @@
-import { Module } from '@nestjs/common';
+import {
+  ZodValidationPipe,
+  ZodSerializerInterceptor,
+  ZodSerializationException,
+} from 'nestjs-zod';
+import {
+  APP_PIPE,
+  APP_INTERCEPTOR,
+  APP_FILTER,
+  BaseExceptionFilter,
+} from '@nestjs/core';
+import { ZodError } from 'zod';
+import {
+  Module,
+  HttpException,
+  ArgumentsHost,
+  Logger,
+  Catch,
+} from '@nestjs/common';
 import { AuthModule } from './auth/auth.module.js';
 import { ConfigModule } from '@nestjs/config';
 import { envSchema } from './config/env.schema.js';
 import { DbService } from './prisma/db.service.js';
 import { DbModule } from './prisma/db.module.js';
+
+@Catch(HttpException)
+class HttpExceptionFilter extends BaseExceptionFilter {
+  private logger = new Logger(HttpExceptionFilter.name);
+
+  catch(exception: HttpException, host: ArgumentsHost) {
+    if (exception instanceof ZodSerializationException) {
+      const zodError = exception.getZodError();
+
+      if (zodError instanceof ZodError) {
+        this.logger.error(`ZodSerializationException: ${zodError.message}`);
+      }
+    }
+
+    super.catch(exception, host);
+  }
+}
 
 @Module({
   imports: [
@@ -15,6 +50,20 @@ import { DbModule } from './prisma/db.module.js';
     DbModule,
   ],
   controllers: [],
-  providers: [DbService],
+  providers: [
+    DbService,
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ZodSerializerInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,5 @@
+import { cleanupOpenApiDoc } from 'nestjs-zod';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ClerkClient, clerkMiddleware } from '@clerk/express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
@@ -5,6 +7,13 @@ import { CLERK_CLIENT } from './auth/clerk.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const openApiDoc = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder().setTitle('Relaybox').setVersion('1.0').build(),
+  );
+
+  SwaggerModule.setup('api', app, cleanupOpenApiDoc(openApiDoc));
   app.use(clerkMiddleware({ clerkClient: app.get<ClerkClient>(CLERK_CLIENT) }));
   await app.listen(process.env.PORT ?? 3000);
 }
